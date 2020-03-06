7fbb20cee12b1a9f7bf56a7ddb016ed8265d793c
Victor Malumbres <41515186+victorK8@users.noreply.github.com>
2020-03-06 16:37:52 +0100

    Functinal USB Reader in python

--- USB_Reader.py
+++ USB_Reader.py
@@ -3,15 +3,13 @@ import serial
 import time
 
 # Open bus 
-bus = serial.Serial('/dev/ttyUSB0', 9600, timeout = 5)
+bus = serial.Serial('/dev/ttyACM0', 9600, timeout = 5)
 
 # Read Bus & Show message for command line
 while True:
-     
-     number_of_input_bytes = bus.in_waiting
 
-     if number_of_input_bytes > 0:
-        
         # Read message until '\n'char 
-        message = bus.read_until()
-        print(message)
+        message = bus.readline()
+
+        print(message.decode('utf8','strict'))
+
