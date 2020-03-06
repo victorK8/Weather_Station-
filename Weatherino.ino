c0f10abc1dfc8057d4640132ef0ef500a8949aac
Victor Malumbres <41515186+victorK8@users.noreply.github.com>
2020-03-06 16:34:57 +0100

    Example of UART use (Arduino Part)

--- Weatherino.ino
+++ Weatherino.ino
@@ -5,6 +5,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.write(" Hi Raspi. I'm Arduino Board ");
+  Serial.write(" Hi Raspi. I'm Arduino Board \n");
   delay(500);
 }
