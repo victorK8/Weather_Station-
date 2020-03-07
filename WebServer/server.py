f7b92a948f616ca90ed6620c8b38ae7a4cc6271a
Victor Malumbres <41515186+victorK8@users.noreply.github.com>
2020-03-07 14:41:30 +0100

    Adding index.html

--- WebServer/server.py
+++ WebServer/server.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+# Index page
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+    
+# Dashboard page
+@app.route('/Dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+# Download page
+@app.route('/Downloads')
+def downloads():
+    return render_template('downloads.html')
+
+# About page
+@app.route('/About')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
