272425cda365499cf6d26578f27b39bf589bea5d
victorK8 <victormalum@gmail.com>
2020-05-09 16:41:17 +0200

    Checked file handlings and implemented the last of a day

--- USB-Reader/USB-Reader.c
+++ USB-Reader/USB-Reader.c
@@ -20,8 +20,9 @@
 #include <errno.h>
 
 
-#define BAUD_RATE 9600
-#define COM '/dev/ttyACM0'
+#define BAUDRATE 9600
+#define COM '/dev/ttyACM0' // COM PORT FOR ARDUINO
+#define DAY_STAMP 86400 // DAY IN SECONDS 
 
 
 
@@ -39,27 +40,26 @@ const char Filename[] = "Raspi_Log_File_";
 // Header
 const char Header[] = "Temperature,Humidity,Timestamp,Date \n";
 
-// Ext.
-const char Ext[] = ".csv";
-
 // time_stamp struct
-time_t TimeStamp;
-
+time_t TimeStamp_0;
 
 
 /* ----------- App ------------ */
 
 int main(int argc, char *argv[]){
+    
+    // Some vars
+    FILE * fp; // File Handling
+    char Path[1024]; // Full Path of current log file
+    time_t TimeStamp; // Timer 
 
     // Request timestamp 
-    time(&TimeStamp);
+    time(&TimeStamp_0);
    
-
     // Concat to filename to Path[]
-    char Path[1024];
-    sprintf(Path, "%s%s%ld%s", Path_To_Storage_LogFiles,Filename,TimeStamp,Ext);
+    sprintf(Path, "%s%s%ld%s", Path_To_Storage_LogFiles, Filename, TimeStamp_0, ".csv");
 
-    // Write header.
+    // Write header
     FILE *fp;
 
     // Open file
@@ -77,8 +77,45 @@ int main(int argc, char *argv[]){
         exit(-1);
     }
 
-    // loop
+    // Loop
     while(1){
+
+
+        // Get curren time
+        time(&TimeStamp);
+
+        // Check last of a day
+        if(((int)difftime(TimeStamp, TimeStamp_0)) > DAY_STAMP){
+
+            // Update stamp marker
+            TimeStamp_0 = TimeStamp;
+
+            // Update Filename
+            sprintf(Path, "%s%s%ld%s", Path_To_Storage_LogFiles, Filename, TimeStamp_0, ".csv");
+
+            // Write header
+            FILE *fp;
+
+            // Open file
+            fp = fopen(Path, "w");
+
+            // Write file
+            if(fprintf(fp, Header) < 0){
+                printf("Error writing in Log-File");
+                exit(-1);
+            }
+
+            // Close file
+            if(fclose(fp) != 0) {
+                printf("Error closing in Log-File");
+                exit(-1);
+            }
+        }
+
+        // Something in COM ?
+        // Read it 
+        // Append to log file
+        // Send to web via TCP/IP
 
     }
 
